642f15da2f6dd9eb9ee9f6d2e4fd54eecc9b8c45
VaheGomktsyan <148968766+VaheGomktsyan@users.noreply.github.com>
2024-10-06 22:30:04 +0400

    Testing

--- src/list/list.controller.spec.ts
+++ src/list/list.controller.spec.ts
@@ -34,6 +34,41 @@ describe('ListController', () => {
     });
   });
 
-
+  describe('GET/:id', () => {
+    it('should be return an object of list', async () => {
+      const newResult: any = {};
+      jest.spyOn(service, 'findOne').mockResolvedValueOnce(newResult);
+      const result = await controller.findOne("1");
+      return expect(result).toBe(newResult);
+    });
+  });
   
+  describe('POST', () => {
+    it('should be add new list', async () => {
+      const newResult: any = {
+        "name": "todo",
+      };
+      jest.spyOn(service, 'create').mockImplementation(() => newResult);
+      const result = await controller.create(newResult);
+      expect(result).toEqual(newResult);
+    });
+  });
+
+  describe('PATCH', () => {
+    it('should be updated correct list', async () => {
+      const newlist: any = {};
+      jest.spyOn(service, 'update').mockImplementation(() => newlist);
+      const result = await controller.update("1", newlist);
+      expect(result).toEqual(newlist);
+    });
+  });
+
+  describe('DELETE', () => {
+    it('should be delete correct list', async () => {
+      const deletelist: any = {};
+      jest.spyOn(service, 'remove').mockImplementation(() => deletelist);
+      const result = await controller.remove("1");
+      expect(result).toEqual(deletelist);
+    });
+  });
 });
